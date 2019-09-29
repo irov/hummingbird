912c62c91cffa0dffe02b1688529d3b1eb36d85a
yuriy.levchenko <irov13@mail.ru>
2019-09-29 21:35:21 +0300

    wip mongo

--- src/hb_mongo/hb_mongo.c
+++ src/hb_mongo/hb_mongo.c
@@ -5,8 +5,9 @@
 
 #include "mongoc/mongoc.h"
 
+//////////////////////////////////////////////////////////////////////////
 mongoc_client_t * g_mongo_client = HB_NULLPTR;
-
+//////////////////////////////////////////////////////////////////////////
 int hb_mongo_initialze( const char * _name, const char * _uri )
 {
     mongoc_init();
@@ -42,7 +43,7 @@ int hb_mongo_initialze( const char * _name, const char * _uri )
 
     return 1;
 }
-
+//////////////////////////////////////////////////////////////////////////
 void hb_mongo_finalize()
 {
     mongoc_client_destroy( g_mongo_client );
@@ -50,7 +51,7 @@ void hb_mongo_finalize()
 
     mongoc_cleanup();
 }
-
+//////////////////////////////////////////////////////////////////////////
 int hb_mongo_get_collection( const char * _db, const char * _name, hb_mongo_collection_handler_t * _collection )
 {
     mongoc_collection_t * collection = mongoc_client_get_collection( g_mongo_client, _db, _name );
@@ -60,6 +61,13 @@ int hb_mongo_get_collection( const char * _db, const char * _name, hb_mongo_coll
     return 1;
 }
 //////////////////////////////////////////////////////////////////////////
+void hb_mongo_collection_destroy( hb_mongo_collection_handler_t * _collection )
+{
+    mongoc_collection_t * mongo_collection = (mongoc_collection_t *)_collection->handler;
+
+    mongoc_collection_destroy( mongo_collection );
+}
+//////////////////////////////////////////////////////////////////////////
 int hb_mongo_get_value( hb_mongo_collection_handler_t * _collection, const char * _id, hb_mongo_value_handler_t * _value )
 {
     mongoc_collection_t * mongo_collection = (mongoc_collection_t *)_collection->handler;
@@ -67,46 +75,64 @@ int hb_mongo_get_value( hb_mongo_collection_handler_t * _collection, const char 
     bson_oid_t oid;
     bson_oid_init_from_string( &oid, _id );
 
-    bson_t * query = BCON_NEW( "$query",
-        "{",
-        "_id",
-        BCON_OID( &oid ),
-        "}" );
+    bson_t query;
+    bson_init( &query );
+    BSON_APPEND_OID( &query, "_id", &oid );
 
-    mongoc_cursor_t * cursor = mongoc_collection_find( mongo_collection, MONGOC_QUERY_NONE, 0, 0, 0, query, NULL, NULL );
+    //bson_t fields;
+    //bson_init( &fields );
+    //BSON_APPEND_INT32( &fields, "value", 1 );
 
-    bson_destroy( query );
+    mongoc_cursor_t * cursor = mongoc_collection_find( mongo_collection, MONGOC_QUERY_NONE, 0, 0, 0, &query, HB_NULLPTR, HB_NULLPTR );
 
-    const bson_t * value;
-    mongoc_cursor_next( cursor, &value );
+    bson_destroy( &query );
+    //bson_destroy( &fields );
 
-    _value->handler1 = cursor;
-    _value->handler2 = value;
-    _value->value = HB_NULLPTR;
+    const bson_t * data;
+    mongoc_cursor_next( cursor, &data );
+
+    //char * str = bson_as_canonical_extended_json( data, NULL );
+    //bson_free( str );
+
+    bson_iter_t iter;
+    if( bson_iter_init( &iter, data ) == false )
+    {
+        mongoc_cursor_destroy( cursor );
+
+        return 0;
+    }
+
+    if( bson_iter_find( &iter, "value" ) == false )
+    {
+        mongoc_cursor_destroy( cursor );
+
+        return 0;
+    }
+
+    uint32_t length;
+    const char * value = bson_iter_utf8( &iter, &length );
+
+    _value->handler = cursor;
+    _value->length = (size_t)length;
+    _value->value = value;
 
     return 1;
 }
 //////////////////////////////////////////////////////////////////////////
 void hb_mongo_value_destroy( hb_mongo_value_handler_t * _value )
 {
-    mongoc_cursor_t * cursor = (mongoc_cursor_t *)_value->handler1;
+    mongoc_cursor_t * cursor = (mongoc_cursor_t *)_value->handler;
 
     mongoc_cursor_destroy( cursor );
 }
 //////////////////////////////////////////////////////////////////////////
-const char * hb_mongo_value_lock( hb_mongo_value_handler_t * _value )
+const char * hb_mongo_value_as_utf8( hb_mongo_value_handler_t * _value, size_t * _length )
 {
-    const bson_t * mongo_value = (const bson_t *)_value->handler2;
+    if( _length != HB_NULLPTR )
+    {
+        *_length = _value->length;
+    }
 
-    const char * str = bson_as_canonical_extended_json( mongo_value, NULL );
-
-    _value->value = str;
-
-    return str;
-}
-//////////////////////////////////////////////////////////////////////////
-void hb_mongo_value_unlock( hb_mongo_value_handler_t * _value )
-{
-    bson_free( (char *)_value->value );
+    return _value->value;
 }
 //////////////////////////////////////////////////////////////////////////
--- src/hb_mongo/hb_mongo.h
+++ src/hb_mongo/hb_mongo.h
@@ -1,6 +1,8 @@
 #ifndef HB_MONGO_H_
 #define HB_MONGO_H_
 
+#include <stdint.h>
+
 int hb_mongo_initialze( const char * _name, const char * _uri );
 void hb_mongo_finalize();
 
@@ -10,18 +12,18 @@ typedef struct hb_mongo_collection_handler_t
 } hb_mongo_collection_handler_t;
 
 int hb_mongo_get_collection( const char * _db, const char * _name, hb_mongo_collection_handler_t * _collection );
+void hb_mongo_collection_destroy( hb_mongo_collection_handler_t * _collection );
 
 typedef struct hb_mongo_value_handler_t
 {
-    const void * handler1;
-    const void * handler2;
+    const void * handler;
+    size_t length;
     const char * value;
 } hb_mongo_value_handler_t;
 
 int hb_mongo_get_value( hb_mongo_collection_handler_t * _collection, const char * _id, hb_mongo_value_handler_t * _value );
 void hb_mongo_value_destroy( hb_mongo_value_handler_t * _value );
 
-const char * hb_mongo_value_lock( hb_mongo_value_handler_t * _value );
-void hb_mongo_value_unlock( hb_mongo_value_handler_t * _value );
+const char * hb_mongo_value_as_utf8( hb_mongo_value_handler_t * _value, size_t * _length );
 
 #endif
