b359f74ad042801968e5a8bfa49ed4e8996b7cc2
irov <irov13@mail.ru>
2019-10-03 12:58:13 +0300

    improve script

--- src/hb_log/hb_log.h
+++ src/hb_log/hb_log.h
@@ -1,10 +1,14 @@
 #ifndef HB_LOG_H_
 #define HB_LOG_H_
 
+#define HB_LOG_ALL 0
 #define HB_LOG_INFO 0
 #define HB_LOG_WARNING 1
 #define HB_LOG_ERROR 2
 #define HB_LOG_CRITICAL 4
+
+int hb_log_initialize();
+void hb_log_finalize();
 
 typedef void(*hb_log_observer_t)(const char * _category, int _level, const char * _message);
 
