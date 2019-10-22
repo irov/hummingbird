86b75cee4487a9aad8525f94f75b59b4e5cdc26a
yuriy.levchenko <irov13@mail.ru>
2019-10-22 14:07:48 +0300

    improve hb_cache

--- src/hb_cache/hb_cache.h
+++ src/hb_cache/hb_cache.h
@@ -6,9 +6,9 @@
 int hb_cache_initialze( const char * _uri, uint32_t _port );
 void hb_cache_finalize();
 
-int hb_cache_set_value( const char * _key, const void * _value, size_t _size );
-int hb_cache_get_value( const char * _key, void * _value, size_t _capacity, size_t * _size );
-int hb_cache_expire_value( const char * _key, uint32_t _seconds );
+int hb_cache_set_value( const void * _key, size_t _keysize, const void * _value, size_t _size );
+int hb_cache_get_value( const void * _key, size_t _keysize, void * _value, size_t _capacity, size_t * _size );
+int hb_cache_expire_value( const void * _key, size_t _keysize, uint32_t _seconds );
 
 
 
