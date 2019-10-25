e6e959f9a5cacf479f0083da125b3a3a07bde193
irov <irov13@mail.ru>
2019-10-25 17:12:45 +0300

    wip login user

--- src/hb_node_loginuser/hb_node_loginuser.h
+++ src/hb_node_loginuser/hb_node_loginuser.h
@@ -3,9 +3,20 @@
 
 #include "hb_config/hb_config.h"
 
+static const uint32_t hb_node_loginuser_magic_number = HB_MAGIC_NUMBER( 'N', 'L', 'U', 'R' );
+static const uint32_t hb_node_loginuser_version_number = 1;
+
 typedef struct hb_node_loginuser_in_t
 {
+    uint32_t magic_number;
+    uint32_t version_number;
+
+    char cache_uri[128];
+    uint32_t cache_port;
+
     char db_uri[128];
+
+    uint16_t pid;
 
     char login[128];
     char password[128];
@@ -14,7 +25,10 @@ typedef struct hb_node_loginuser_in_t
 
 typedef struct hb_node_loginuser_out_t
 {
-    uint8_t token[12];
+    uint32_t magic_number;
+    uint32_t version_number;
+
+    hb_token_t token;
 
     uint32_t exist;
 } hb_node_loginuser_out_t;
