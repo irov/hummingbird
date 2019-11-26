908d1c49b02399ce47475cc827468b69e5f024da
irov <irov13@mail.ru>
2019-11-26 14:32:11 +0200

    fix unix time

--- src/hb_utils/hb_time_unix.c
+++ src/hb_utils/hb_time_unix.c
@@ -1,10 +1,11 @@
 #include "hb_time.h"
 
+#define _LARGE_TIME_API
 #include <time.h>
 
 void hb_time( hb_time_t * _time )
 {
-    __time64_t time = _time64( HB_NULLPTR );
+    time64_t time = time64( HB_NULLPTR );
 
     *_time = (hb_time_t)time;
 }
