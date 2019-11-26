bbc2368b6ecaffb7f120b23eeb409acd5714fc57
irov <irov13@mail.ru>
2019-11-26 14:09:24 +0200

    fix hb_base16

--- src/hb_utils/hb_base16.h
+++ src/hb_utils/hb_base16.h
@@ -6,7 +6,7 @@
 size_t hb_base16_encode_size( size_t _size );
 size_t hb_base16_decode_size( size_t _size );
 
-int hb_base16_encode( const void * _binary, size_t _size, char * _base16, size_t _capacity, size_t * _outsize );
-int hb_base16_decode( const char * _base16, size_t _base16size, void * _binary, size_t _capacity, size_t * _outsize );
+hb_result_t hb_base16_encode( const void * _binary, size_t _size, char * _base16, size_t _capacity, size_t * _outsize );
+hb_result_t hb_base16_decode( const char * _base16, size_t _base16size, void * _binary, size_t _capacity, size_t * _outsize );
 
 #endif
