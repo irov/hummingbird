bd06c17c0ab17f12776be3a77fe06f8ab637f038
irov <irov13@mail.ru>
2019-11-26 14:01:54 +0200

    fix unix build

--- build/unix/build_solution.bash
+++ build/unix/build_solution.bash
@@ -4,6 +4,6 @@ CONFIGURATION=$1
 
 mkdir -p ../../solutions/solution_unix/$CONFIGURATION
 pushd ../../solutions/solution_unix/$CONFIGURATION
-cmake -G "Unix Makefiles" -S "../../../cmake/unix" -DCMAKE_BUILD_TYPE:STRING=$CONFIGURATION
+cmake -G "Unix Makefiles" -S "../../../cmake/hummingbird_unix" -DCMAKE_BUILD_TYPE:STRING=$CONFIGURATION
 cmake --build ./ --config $CONFIGURATION
 popd
