f9a79babce7e2fc73acfbb9034526751db7f8031
irov <irov13@mail.ru>
2019-11-26 14:51:59 +0200

    fix

--- src/hb_utils/hb_time_unix.c
+++ src/hb_utils/hb_time_unix.c
@@ -4,7 +4,7 @@
 
 void hb_time( hb_time_t * _time )
 {
-    time_t time = time( HB_NULLPTR );
+    time_t t = time( HB_NULLPTR );
 
-    *_time = (hb_time_t)time;
+    *_time = (hb_time_t)t;
 }
--- src/hb_utils/hb_time_win32.c
+++ src/hb_utils/hb_time_win32.c
@@ -4,7 +4,7 @@
 
 void hb_time( hb_time_t * _time )
 {
-    __time64_t time = _time64( HB_NULLPTR );
+    __time64_t t = _time64( HB_NULLPTR );
 
-    *_time = (hb_time_t)time;
+    *_time = (hb_time_t)t;
 }
