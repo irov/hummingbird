4c1b81deea7cd881b3b703ea1c202eb0c0f1512b
irov <irov13@mail.ru>
2019-11-26 18:52:53 +0200

    remove debug

--- src/hb_node_main/hb_node_main.c
+++ src/hb_node_main/hb_node_main.c
@@ -12,8 +12,6 @@
 #include <stdlib.h>
 #include <stdio.h>
 
-#include <Windows.h>
-
 //////////////////////////////////////////////////////////////////////////
 static void __hb_log_observer( const char * _category, hb_log_level_e _level, const char * _message )
 {
@@ -24,8 +22,6 @@ static void __hb_log_observer( const char * _category, hb_log_level_e _level, co
 //////////////////////////////////////////////////////////////////////////
 int main( int _argc, char * _argv[] )
 {
-    MessageBoxA( NULL, "gdfg", "fsd", MB_OK );
-
     if( hb_log_initialize() == HB_FAILURE )
     {
         return EXIT_FAILURE;
