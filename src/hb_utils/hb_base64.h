7f5ea993294988ee11a1f017c3519e65e87f8105
irov <irov13@mail.ru>
2019-11-26 14:10:10 +0200

    fix hb_base64

--- src/hb_utils/hb_base64.h
+++ src/hb_utils/hb_base64.h
@@ -6,7 +6,7 @@
 size_t hb_base64_encode_size( size_t _size );
 size_t hb_base64_decode_size( size_t _size );
 
-int hb_base64_encode( const void * _binary, size_t _size, char * _base64, size_t _capacity, size_t * _outsize );
-int hb_base64_decode( const char * _base64, size_t _base64size, void * _binary, size_t _capacity, size_t * _outsize );
+hb_result_t hb_base64_encode( const void * _binary, size_t _size, char * _base64, size_t _capacity, size_t * _outsize );
+hb_result_t hb_base64_decode( const char * _base64, size_t _base64size, void * _binary, size_t _capacity, size_t * _outsize );
 
 #endif
