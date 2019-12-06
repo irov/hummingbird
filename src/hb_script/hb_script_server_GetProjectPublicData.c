f9e33d039abbd83e792f4ef01bcc4569a60e0355
irov <irov13@mail.ru>
2019-12-06 21:13:15 +0200

    wip User|Project Entity

---
+++ src/hb_script/hb_script_server_GetProjectPublicData.c
@@ -0,0 +1,51 @@
+#include "hb_script_handle.h"
+#include "hb_script_json.h"
+
+#include "hb_json/hb_json.h"
+
+//////////////////////////////////////////////////////////////////////////
+extern hb_script_handle_t * g_script_handle;
+//////////////////////////////////////////////////////////////////////////
+int __hb_script_server_GetProjectPublicData( lua_State * L )
+{
+    const char * fields[16];
+
+    uint32_t field_iterator = 0;
+
+    lua_pushvalue( L, 1 );
+    lua_pushnil( L );
+    while( lua_next( L, -2 ) != 0 )
+    {
+        const char * value = lua_tostring( L, -1 );
+        fields[field_iterator++] = value;
+
+        lua_pop( L, 1 );
+    }
+    lua_pop( L, 1 );
+
+    const char * db_fields[1] = { "public_data" };
+
+    hb_db_value_handle_t handler[1];
+    if( hb_db_get_values( g_script_handle->db_collection_projects, g_script_handle->project_oid, db_fields, 1, handler ) == HB_FAILURE )
+    {
+        lua_pushboolean( L, 0 );
+
+        for( uint32_t index = 0; index != field_iterator; ++index )
+        {
+            lua_pushnil( L );
+        }
+
+        return 1 + field_iterator;
+    }
+
+    lua_pushboolean( L, 1 );
+
+    if( hb_script_json_load_fields( L, handler[0].u.symbol.buffer, handler[0].u.symbol.length, fields, field_iterator ) == HB_FAILURE )
+    {
+        return -1;
+    }
+
+    hb_db_destroy_values( handler, 1 );
+
+    return 1 + field_iterator;
+}
---
+++ src/hb_script/hb_script_server_SetProjectPublicData.c
@@ -0,0 +1,28 @@
+#include "hb_script_handle.h"
+#include "hb_script_json.h"
+
+#include "hb_json/hb_json.h"
+
+//////////////////////////////////////////////////////////////////////////
+extern hb_script_handle_t * g_script_handle;
+//////////////////////////////////////////////////////////////////////////
+int __hb_script_server_SetProjectPublicData( lua_State * L )
+{
+    char json_data[2048];
+    size_t json_data_size;
+    hb_script_json_dumps( L, 1, json_data, 2048, &json_data_size );
+
+    hb_db_value_handle_t handler[1];
+    hb_db_make_symbol_value( "public_data", ~0U, json_data, json_data_size, handler + 0 );
+
+    if( hb_db_update_values( g_script_handle->db_collection_projects, g_script_handle->project_oid, handler, 1 ) == HB_FAILURE )
+    {
+        lua_pushboolean( L, 0 );
+
+        return 1;
+    }
+
+    lua_pushboolean( L, 1 );
+
+    return 1;
+}
