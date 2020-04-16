06635e3598ad13b5f59020029ad9e67f60b01d28
irov <irov13@mail.ru>
2020-04-17 01:38:03 +0300

    update

--- build/msvc16/make_solution.bat
+++ build/msvc16/make_solution.bat
@@ -10,3 +10,5 @@ set "BUILD_TEMP_DIR=%~dp0..\..\solutions\solution_msvc16\%CONFIGURATION%"
 call cmake -G "Visual Studio 16 2019" -A Win32 -S "%CD%\..\..\..\cmake\hummingbird_win32" -DCMAKE_CONFIGURATION_TYPES:STRING=%CONFIGURATION% -DCMAKE_BUILD_TYPE:STRING=%CONFIGURATION%
 
 @popd
+
+@pause
