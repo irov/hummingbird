c7af166eb84add52a5624960c88473debbaa7e46
irov <irov13@mail.ru>
2020-05-15 17:22:11 +0300

    improve hb_mutex_unix

--- src/hb_mutex/hb_mutex_unix.c
+++ src/hb_mutex/hb_mutex_unix.c
@@ -1,65 +1,61 @@
-#include "hb_thread.h"
+#include "hb_mutex.h"
 
 #include "hb_log/hb_log.h"
 
-#include <unistd.h>
+#include <errno.h>
 #include <pthread.h>
 
 //////////////////////////////////////////////////////////////////////////
-typedef struct hb_thread_proxy_t
+typedef struct hb_mutex_handle_t
 {
-    hb_thread_function_t function;
-    void * ud;
-}hb_thread_proxy_t;
+    pthread_mutex_t id;
+}hb_mutex_handle_t;
 //////////////////////////////////////////////////////////////////////////
-typedef struct hb_thread_handle_t
+hb_result_t hb_mutex_create( hb_mutex_handle_t ** _handle )
 {
-    pthread_t id;
-    hb_thread_proxy_t * proxy;
-}hb_thread_handle_t;
-//////////////////////////////////////////////////////////////////////////
-static void * __hb_thread_proxy( void * _ud )
-{
-    hb_thread_proxy_t * proxy = (hb_thread_proxy_t *)(_ud);
+    hb_mutex_handle_t * handle = HB_NEW( hb_mutex_handle_t );
 
-    (*proxy->function)(proxy->ud);
+    pthread_mutexattr_t attr;
+    pthread_mutexattr_init( &attr );
+    pthread_mutexattr_settype( &attr, PTHREAD_MUTEX_RECURSIVE );
 
-    pthread_exit( HB_NULLPTR );
-}
-//////////////////////////////////////////////////////////////////////////
-hb_result_t hb_thread_create( hb_thread_function_t _function, void * _ud, hb_thread_handle_t ** _handle )
-{
-    hb_thread_proxy_t * proxy = HB_NEW( hb_thread_proxy_t );
-    proxy->function = _function;
-    proxy->ud = _ud;
-
-    pthread_t id;
-    pthread_create( &id, HB_NULLPTR, &__hb_thread_proxy, proxy );
-    
-    hb_thread_handle_t * handle = HB_NEW( hb_thread_handle_t );
+    pthread_mutex_t id;
+    if( pthread_mutex_init( &id, &attr ) != 0 )
+    {
+        return HB_FAILURE;
+    }
 
     handle->id = id;
-    handle->proxy = proxy;
 
     *_handle = handle;
 
     return HB_SUCCESSFUL;
 }
 //////////////////////////////////////////////////////////////////////////
-void hb_thread_join( hb_thread_handle_t * _handle )
+void hb_mutex_destroy( hb_mutex_handle_t * _handle )
 {
-    pthread_t id = _handle->id;
+    pthread_mutex_destroy( _handle->id );
 
-    pthread_join( id, HB_NULLPTR );
-}
-//////////////////////////////////////////////////////////////////////////
-void hb_thread_destroy( hb_thread_handle_t * _handle )
-{
-    pthread_t id = _handle->id;
-
-    pthread_cancel( id );
-    pthread_join( id, HB_NULLPTR );
-
-    HB_DELETE( _handle->proxy );
     HB_DELETE( _handle );
 }
+//////////////////////////////////////////////////////////////////////////
+hb_bool_t hb_mutex_try_lock( hb_mutex_handle_t * _handle )
+{
+    if( pthread_mutex_trylock( _handle->id ) == EBUSY )
+    {
+        return HB_FALSE;
+    }
+
+    return HB_TRUE;
+}
+//////////////////////////////////////////////////////////////////////////
+void hb_mutex_lock( hb_mutex_handle_t * _handle )
+{
+    pthread_mutex_lock( _handle->id );
+}
+//////////////////////////////////////////////////////////////////////////
+void hb_mutex_unlock( hb_mutex_handle_t * _handle )
+{
+    pthread_mutex_unlock( _handle->id );
+}
+//////////////////////////////////////////////////////////////////////////
