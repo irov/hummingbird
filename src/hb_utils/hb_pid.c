eeaa94354c03bb1d10fb86f5d9e7be25fc94a363
yuriy.levchenko <irov13@mail.ru>
2020-05-18 13:15:07 +0300

    some fix

--- src/hb_utils/hb_pid.c
+++ src/hb_utils/hb_pid.c
@@ -10,9 +10,9 @@ hb_result_t hb_pid_base16_encode( const hb_pid_t _pid, hb_pid16_t * _base16 )
     return result;
 }
 //////////////////////////////////////////////////////////////////////////
-hb_result_t hb_pid_base16_decode( const hb_pid16_t _base16, hb_pid_t * _pid )
+hb_result_t hb_pid_base16_decode( const hb_pid16_t * _base16, hb_pid_t * _pid )
 {
-    hb_result_t result = hb_base16_decode( _base16.value, sizeof( hb_pid16_t ), _pid, sizeof( hb_pid_t ), HB_NULLPTR );
+    hb_result_t result = hb_base16_decode( _base16->value, sizeof( hb_pid16_t ), _pid, sizeof( hb_pid_t ), HB_NULLPTR );
 
     return result;
 }
