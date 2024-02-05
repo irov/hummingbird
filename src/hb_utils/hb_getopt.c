ce9632afb3aa9ddf0be9dff09654e35542d53bb3
irov <irov13@mail.ru>
2024-02-05 21:39:31 +0200

    add hb_getopti

--- src/hb_utils/hb_getopt.c
+++ src/hb_utils/hb_getopt.c
@@ -1,7 +1,9 @@
 #include "hb_getopt.h"
 
 #include <string.h>
+#include <stdlib.h>
 
+//////////////////////////////////////////////////////////////////////////
 hb_result_t hb_getopt( int argc, char * argv[], const char * _name, const char ** _value )
 {
     for( int index = 1; index != argc; ++index )
@@ -23,3 +25,25 @@ hb_result_t hb_getopt( int argc, char * argv[], const char * _name, const char *
 
     return HB_FAILURE;
 }
+//////////////////////////////////////////////////////////////////////////
+hb_result_t hb_getopti( int argc, char * argv[], const char * _name, int64_t * const _value )
+{
+    const char * value;
+    if( hb_getopt( argc, argv, _name, &value ) == HB_FAILURE )
+    {
+        return HB_FAILURE;
+    }
+
+    char * endptr;
+    int64_t v = strtoll( value, &endptr, 0 );
+
+    if( *endptr != '\0' )
+    {
+        return HB_FAILURE;
+    }
+
+    *_value = v;
+
+    return HB_SUCCESSFUL;
+}
+//////////////////////////////////////////////////////////////////////////
--- src/hb_utils/hb_getopt.h
+++ src/hb_utils/hb_getopt.h
@@ -4,5 +4,6 @@
 #include "hb_config/hb_config.h"
 
 hb_result_t hb_getopt( int argc, char * argv[], const char * _name, const char ** _value );
+hb_result_t hb_getopti( int argc, char * argv[], const char * _name, int64_t * const _value );
 
 #endif
